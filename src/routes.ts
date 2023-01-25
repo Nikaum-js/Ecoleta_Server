242d13b6c5b8c95ae7605c80a3f299a59ef8dcf3
Nikolas-as <nikkolas21082003@gmail.com>
2023-01-25 12:45:29 -0300

    create rotes and refactoring code :refactor:

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 
-const routes = express.Router();
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
 
-routes.post('/', (request, response) => {
-  return response.json({ message: 'Óla' })
-})
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index)
+
+routes.get('/points/:id', pointsController.show)
+routes.get('/points', pointsController.index)
+routes.post('/points', pointsController.create)
 
 export default routes
