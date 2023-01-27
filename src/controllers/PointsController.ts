8d9f8308b139d8fb99614cf2b08e4ba0fdcc4d18
Nikolas-as <nikkolas21082003@gmail.com>
2023-01-27 16:13:50 -0300

    create contollers :tada:

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -35,7 +35,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1604719312566-8912e9227c6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
